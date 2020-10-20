67987fc2b7e35c07f33dca46d8c6baf5a78499c3
Xandre Du Plooy <xduplooy@gmail.com>
2020-10-21 12:01:32 +1300

    Inital database and server setup

---
+++ index.js
@@ -0,0 +1,44 @@
+const { ApolloServer } = require('apollo-server')
+const gql = require('graphql-tag')
+const mongoose = require('mongoose')
+
+const Post = require('./models/Post')
+const { MONGODB } = require('./config.js')
+
+const typeDefs = gql`
+	type Post{
+		id: ID!,
+		body: String!,
+		createdAt: String!,
+		username: String!
+	}
+	type Query{
+		getPosts: [Post]
+	}
+`
+
+const resolvers = {
+	Query: {
+		async getPosts() {
+			try {
+				const posts = await Post.find()
+				return posts
+			} catch(error) {
+				throw new Error(error)
+			}
+		}
+	}
+}
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers
+})
+
+mongoose.connect(MONGODB, {useNewUrlParser: true})
+	.then(() => {
+		console.log('MONGODB connected')
+		return server.listen({port: 5000})
+	}).then(res => {
+		console.log(`server running at ${res.url}`)
+	})
