e1535d924e0f35407b97694439c1a7e2347dc7fe
Xandre Du Plooy <xduplooy@gmail.com>
2020-11-23 18:49:28 +1300

    displaying posts

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Card, Icon, Label, Image, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import moment from 'moment'
+
+const PostCard = ({ post: { body, createdAt, username, id, likeCount, commentCount, likes } }) => {
+
+	const likePost = () => {
+		console.log('like post')
+	}
+	const commentOnPost = () => {
+		console.log('comment post')
+	}
+
+	return (
+		<Card fluid>
+			<Card.Content>
+				<Image
+					floated='right'
+					size='mini'
+					src='https://react.semantic-ui.com/images/avatar/large/molly.png'
+				/>
+				<Card.Header>{username}</Card.Header>
+				<Card.Meta as={Link} to={`/posts/${id}`}>{moment(createdAt).fromNow()}</Card.Meta>
+				<Card.Description>
+					{body}
+				</Card.Description>
+			</Card.Content>
+			<Card.Content extra>
+				<Button as='div' labelPosition='right' onClick={likePost}>
+					<Button color='teal' basic>
+						<Icon name='heart' />
+					</Button>
+					<Label basic color='teal' pointing='left'>
+						{likeCount}
+					</Label>
+				</Button>
+				<Button as='div' labelPosition='right' onClick={commentOnPost}>
+					<Button color='blue' basic>
+						<Icon name='comments' />
+					</Button>
+					<Label basic color='blue' pointing='left'>
+						{commentCount}
+					</Label>
+				</Button>
+			</Card.Content>
+		</Card>
+	)
+}
+
+export default PostCard
