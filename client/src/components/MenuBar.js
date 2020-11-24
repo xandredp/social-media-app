3cbf8ad1a9aa46f205c814148c9daaf910f55aeb
Xandre Du Plooy <xduplooy@gmail.com>
2020-11-24 17:52:22 +1300

    Render menu bar based on user logged in or not

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,15 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
+import { AuthContext } from '../context/auth';
+
 const MenuBar = () => {
+	const { user, logout } = useContext(AuthContext)
 	const pathname = window.location.pathname
 	const path = pathname === '/' ? 'home' : pathname.substr(1)
 	const [activeItem, setActiveItem] = useState(path)
-
 	const handleItemClick = (e, { name }) => setActiveItem(name)
 
-	return (
+	const menuBar = user ? (
+		// Render a logged in menu bar
+		<Menu pointing secondary size='massive' color='teal'>
+			<Menu.Item
+				name={user.username}
+				active
+				as={Link}
+				to='/'
+			/>
+			<Menu.Menu position='right'>
+				<Menu.Item
+					name='logout'
+					onClick={logout}
+				/>
+			</Menu.Menu>
+		</Menu>
+	) : (
+		// Render a logged out menu bar
 		<Menu pointing secondary size='massive' color='teal'>
 			<Menu.Item
 				name='home'
@@ -36,6 +55,8 @@ const MenuBar = () => {
 			</Menu.Menu>
 		</Menu>
 	)
+
+	return menuBar
 }
 
 export default MenuBar
