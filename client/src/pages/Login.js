d49fb76b19f5f6e7aa64b00e7c6b752f0779eb31
Xandre Du Plooy <xduplooy@gmail.com>
2020-11-24 17:51:48 +1300

    user context

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { Form, Button } from 'semantic-ui-react';
 import { gql, useMutation } from '@apollo/client'
 
-import {useForm} from '../util/hooks';
+import { AuthContext } from '../context/auth';
+import { useForm } from '../util/hooks';
 
 const Login = (props) => {
-	const [errors, setErrors] = useState({});
+	const context = useContext(AuthContext)
+	const [errors, setErrors] = useState({})
 
 	const { onChange, onSubmit, values } = useForm(loginUserCallback, {
 		username: '',
@@ -13,7 +15,8 @@ const Login = (props) => {
 	})
 
 	const [loginUser, { loading }] = useMutation(LOGIN_USER, {
-		update(_, result) {
+		update(_, { data: { login: userData } }) {
+			context.login(userData)
 			props.history.push('/')
 		},
 		onError(err) {
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { Form, Button } from 'semantic-ui-react';
 import { gql, useMutation } from '@apollo/client'
 
+import { AuthContext } from '../context/auth';
 import { useForm } from '../util/hooks';
 
 const Register = (props) => {
+	const context = useContext(AuthContext)
 	const [errors, setErrors] = useState({});
 
 	const { onChange, onSubmit, values } = useForm(registerUser, {
@@ -15,7 +17,8 @@ const Register = (props) => {
 	})
 
 	const [addUser, { loading }] = useMutation(REGISTER_USER, {
-		update(_, result) {
+		update(_, { data: { register: userData } }) {
+			context.login(userData)
 			props.history.push('/')
 		},
 		onError(err) {
