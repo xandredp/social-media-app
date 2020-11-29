4e72cffc7008289016ea0e4fbbff35d88a7a99c7
Xandre Du Plooy <xduplooy@gmail.com>
2020-11-29 19:37:04 +1300

    liking a post and LikeButton component

---
+++ client/src/components/LikeButton.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import { Icon, Label, Button } from 'semantic-ui-react';
+import { gql, useMutation } from '@apollo/client'
+
+const LikeButton = ({ user, post: { id, likeCount, likes } }) => {
+	const [liked, setLiked] = useState(false)
+
+	useEffect(() => {
+		if (user && likes.find(like => like.username === user.username)) {
+			setLiked(true)
+		} else setLiked(false)
+	}, [user, likes])
+
+	const [likePost] = useMutation(LIKE_POST_MUTATION, {
+		variables: { postId: id }
+	})
+
+	const likeButton = user ? (
+		liked ? (
+			<Button color='teal'>
+				<Icon name='heart' />
+			</Button>
+		) : (
+				<Button color='teal' basic>
+					<Icon name='heart' />
+				</Button>
+			)
+	) : (
+			<Button as={Link} to='/login' color='teal' basic>
+				<Icon name='heart' />
+			</Button>
+		)
+
+	return (
+		<Button as='div' labelPosition='right' onClick={likePost}>
+			{likeButton}
+			<Label basic color='teal' pointing='left'>
+				{likeCount}
+			</Label>
+		</Button>
+	)
+}
+
+const LIKE_POST_MUTATION = gql`
+	mutation likePost($postId: ID!) {
+		likePost(postId: $postId) {
+			id
+			likes {
+				id
+				username
+			}
+			likeCount
+		}
+	}
+`
+
+export default LikeButton
--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,16 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Card, Icon, Label, Image, Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import moment from 'moment'
 
-const PostCard = ({ post: { body, createdAt, username, id, likeCount, commentCount, likes } }) => {
+import { AuthContext } from '../context/auth';
+import LikeButton from './LikeButton'
 
-	const likePost = () => {
-		console.log('like post')
-	}
-	const commentOnPost = () => {
-		console.log('comment post')
-	}
+const PostCard = ({ post: { body, createdAt, username, id, likeCount, likes, commentCount } }) => {
+	const {user} = useContext(AuthContext)
 
 	return (
 		<Card fluid>
@@ -27,15 +24,8 @@ const PostCard = ({ post: { body, createdAt, username, id, likeCount, commentCou
 				</Card.Description>
 			</Card.Content>
 			<Card.Content extra>
-				<Button as='div' labelPosition='right' onClick={likePost}>
-					<Button color='teal' basic>
-						<Icon name='heart' />
-					</Button>
-					<Label basic color='teal' pointing='left'>
-						{likeCount}
-					</Label>
-				</Button>
-				<Button as='div' labelPosition='right' onClick={commentOnPost}>
+				<LikeButton user={user} post={{ id, likes, likeCount }}/>
+				<Button labelPosition='right' as={Link} to={`/posts/${id}`}>
 					<Button color='blue' basic>
 						<Icon name='comments' />
 					</Button>
@@ -43,6 +33,13 @@ const PostCard = ({ post: { body, createdAt, username, id, likeCount, commentCou
 						{commentCount}
 					</Label>
 				</Button>
+				{
+					user && user.username === username && (
+						<Button as='div' color='red' floated='right' onClick={() => console.log('delete post')}>
+							<Icon name='trash' style={{ margin: 0 }}/>
+						</Button>
+					)
+				}
 			</Card.Content>
 		</Card>
 	)
